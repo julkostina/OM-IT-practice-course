f481698518ff0d10f6fe801fe84a604662f2705a
julkostina <julikostina1504@gmail.com>
2025-03-03 16:49:09 +0200

    40-43. Creating-filter-of-monsters
    Notes: not modify the initial list, use filter(), include() methods

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,27 +1,50 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters:[],
+      monsters: [],
+      searchField: "",
     };
-    console.log(1);
   }
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState(()=>{return{ monsters: users}},()=>{console.log(this.state.monsters)}));
-    console.log(3);
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state.monsters);
+          }
+        )
+      );
   }
   render() {
-    console.log(2);
+    const filterMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-          return  <div key={monster.id}><h1>{monster.name}</h1></div>
+        <input
+          type="search"
+          className="search-box"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const searchField = event.target.value.toLowerCase();
+            this.setState(()=>{return {searchField}});
+
+          }}
+        />
+        {filterMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
