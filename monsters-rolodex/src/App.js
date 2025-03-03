51d0e2215c94efc1ad1c2e250559fba07637979b
julkostina <julikostina1504@gmail.com>
2025-03-03 15:11:24 +0200

    35.Adding-key-for-maping
    Discussed vulnerability of key in map function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -3,35 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-    monsters:[ {
-      name:'Linda',
-     },
-      {
-        name:'Frank',
-      },
-      {
-        name:'Jacky',
-      },
-    {
-      name:'Andrei',
-    }
-    ]
+      monsters: [
+        {
+          name: "Linda",
+          id:"ef041",
+        },
+        {
+          name: "Frank",
+          id: "284h4",
+        },
+        {
+          name: "Jacky",
+          id: "ef043",
+        },
+        {
+          name: "Andrei",
+          id: "ef044",
+        },
+      ],
     };
   }
-  render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        {
-          this.state.monsters.map(monster => <h1>{monster.name}</h1>) 
-        }
-      </header>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return  <div key={monster.id}><h1>{monster.name}</h1></div>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
