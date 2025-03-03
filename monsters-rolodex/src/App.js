0e3b72c46e1f7afa91dfdd99500289226a7178e1
julkostina <julikostina1504@gmail.com>
2025-03-03 14:57:55 +0200

    33.Mapping-elements-to-objects
    There was consider use of map and why it is better to use an array of objects instead of object with objects

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,27 +6,28 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      name:{firstName: 'Andrei', lastName: 'Zhang'},
-      company: 'ZTM'
+    monsters:[ {
+      name:'Linda',
+     },
+      {
+        name:'Frank',
+      },
+      {
+        name:'Jacky',
+      },
+    {
+      name:'Andrei',
     }
+    ]
+    };
   }
   render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi {this.state.name.firstName} {this.state.name.lastName} , I work at {this.state.company}
-        </p>
-        <button onClick={()=>{
-          this.setState(()=>{
-            return {
-              name: {firstName: 'John', lastName: 'Doe'}
-            }
-          }, ()=>{
-            console.log(this.state);
-          });
-        }}>Change name</button>
+        {
+          this.state.monsters.map(monster => <h1>{monster.name}</h1>) 
+        }
       </header>
     </div>
   );
