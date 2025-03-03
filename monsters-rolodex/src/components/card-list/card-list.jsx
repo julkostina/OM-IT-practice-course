858017e1d497435e8afca560ecdcfe92f3270b68
julkostina <julikostina1504@gmail.com>
2025-03-03 18:07:41 +0200

    51.CardList-component
    CardList component created

--- monsters-rolodex/src/components/card-list/card-list.jsx
+++ monsters-rolodex/src/components/card-list/card-list.jsx
@@ -4,11 +4,14 @@ class CardList extends Component {
   render() {
     const { monsters } = this.props;
     return (
-      <div>
+      <div className="card-list">
         {monsters.map((monster) => {
+          const { id, name, email } = monster;
           return (
-            <div key={monster.id}>
-              <h1 key={monster.id}>{monster.name}</h1>
+            <div className="card-container" key={id}>
+              <img alt={`monster ${name}`} src={`https://robohash.org/${id}`}/>
+              <h2 key={id}>{name}</h2>
+              <p>{email}</p>
             </div>
           );
         })}
