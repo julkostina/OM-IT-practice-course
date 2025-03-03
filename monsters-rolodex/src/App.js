c799a51eaa287365ea0323ff46bf81176e889d70
julkostina <julikostina1504@gmail.com>
2025-03-03 21:47:03 +0200

    58.useState
    adding useState to the App() for handling input

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,22 +1,26 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./App.css";
-import CardList from './components/card-list/card-list.jsx'
-import SearchBox from './components/search-box/search-box.jsx'
-const App=()=>{
-  // const { monsters, searchField } = this.state;
-  //   const { onSearchChange} = this;
-  //   const filterMonsters = monsters.filter((monster) => {
-  //     return monster.name.toLocaleLowerCase().includes(searchField);
-  //   });
-  return(
-    
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        {/* <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='search-box' />
-        <CardList monsters={filterMonsters} /> */}
-      </div>
-    );
-}
+import CardList from "./components/card-list/card-list.jsx";
+import SearchBox from "./components/search-box/search-box.jsx";
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  console.log(searchField);
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+  };
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+        className="search-box"
+      />
+      {/* <CardList monsters={filterMonsters} /> */}
+    </div>
+  );
+};
 // class App extends Component {
 //   constructor() {
 //     super();
