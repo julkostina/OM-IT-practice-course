8e7c8277940ad2787d39aebc15090e1f159a8eb9
julkostina <julikostina1504@gmail.com>
2025-03-04 16:17:01 +0200

    94-96.Setting-up-user-documents
    Here I got knowledge how to get user information using getDoc and  add user to the database using setDoc

--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -1,5 +1,11 @@
 import {initializeApp} from 'firebase/app'
 import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithRedirect} from 'firebase/auth';
+import {
+    getFirestore,
+    getDoc,
+    setDoc,
+    doc
+} from 'firebase/firestore';
 const firebaseConfig = {
 
     apiKey: "AIzaSyAARxGqVBKmMCVQS5KvgNlwWLCENWgUI7M",
@@ -21,3 +27,26 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth) =>{
+    const userDocRef  = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+    const userSnapShot = await getDoc(userDocRef);
+    if(!userSnapShot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+        try{
+            await setDoc(userDocRef,{
+                displayName,
+                email,
+                createdAt,
+            });
+        }
+        catch(error){
+            console.log('Error creating the user', error.message);
+        }
+    }
+    return userDocRef;  
+  }
