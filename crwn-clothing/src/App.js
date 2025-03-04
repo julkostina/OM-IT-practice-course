96a680aab17484e1a0e5b973606af3c1ad0f04d8
julkostina <julikostina1504@gmail.com>
2025-03-04 11:46:06 +0200

    76-77.Categories-and-SASS
    Categories list was created and added sass to the project

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,36 +1,44 @@
-
+import './categories.styles.scss'
+const categories = [
+  {
+    title: 'Hats',
+    id: 1,
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png'
+  },
+  {
+    title: 'Jackets',
+    id: 2,
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png'
+  },
+  {
+    title: 'Sneakers',
+    id: 3,
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png'
+  },
+  {
+    title: 'Womens',
+    id: 4,
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png'
+  },
+  {
+    title: 'Mens',
+    id: 5,
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'
+  },
+]
 
 const  App = ()=> {
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img/> */}
-          <div className="category-body-container"> 
-            <h2>Hats</h2>
-            <p>Shop Now</p>
-          </div>
-           {/* <img/> */}
-          <div className="category-body-container"> 
-            <h2>Jackets</h2>
-            <p>Shop Now</p>
-             {/* <img/> */}
-          </div>
-           {/* <img/> */}
-          <div className="category-body-container"> 
-            <h2>Sneakers</h2>
-            <p>Shop Now</p>
-          </div>
-           {/* <img/> */}
-          <div className="category-body-container"> 
-            <h2>Womens</h2>
-            <p>Shop Now</p>
-          </div>
-           {/* <img/> */}
-          <div className="category-body-container"> 
-            <h2>Mens</h2>
+      {categories.map(({title, id, imageUrl}) => (      
+      <div key={id}className="category-container" >
+          <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
+         <div className="category-body-container"> 
+            <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
       </div>
+      ))}
     </div>
   );
 }
