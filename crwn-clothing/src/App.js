305a3e6a4ccf8f9d9d10c3635074f7bced753827
julkostina <julikostina1504@gmail.com>
2025-03-04 12:23:41 +0200

    78-80.Adding-components
    Added CategoryItem and CategoryList components

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,4 +1,4 @@
-import './categories.styles.scss'
+import CategoriesList from "./components/categories-list/categories-list.component";
 const categories = [
   {
     title: 'Hats',
@@ -29,18 +29,7 @@ const categories = [
 
 const  App = ()=> {
   return (
-    <div className="categories-container">
-      {categories.map(({title, id, imageUrl}) => (      
-      <div key={id}className="category-container" >
-          <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
-         <div className="category-body-container"> 
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-      </div>
-      ))}
-    </div>
+    <CategoriesList categories={categories}/>
   );
 }
-
 export default App;
