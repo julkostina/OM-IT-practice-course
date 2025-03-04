f3eb9bfc9b1b37874a0cf8d8b387a1d2e8f7e086
julkostina <julikostina1504@gmail.com>
2025-03-04 14:50:24 +0200

    89.Setting-up-firebase
    Firebase library added and sign-in component, created account on Firebase

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,7 +1,7 @@
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import { Route, Routes } from "react-router-dom";
-
+import SignIn from "./routes/sign-in/sign-in.component";
 const Shop = () => {
   return (
     <div>
@@ -15,6 +15,8 @@ const  App = ()=> {
       <Route path="/" element={<Navigation />}> 
         <Route index element={<Home />}/>
         <Route path="shop" element={<Shop />}/>
+        <Route path="sign-in" element={<SignIn />}/>
+
       </Route>
 
     </Routes>
