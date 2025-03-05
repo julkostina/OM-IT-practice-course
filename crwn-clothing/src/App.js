ca03175fbf3b9aee0e6baae7dc906299fd877e33
julkostina <julikostina1504@gmail.com>
2025-03-05 17:41:50 +0200

    114.Products-context
    creating context for products

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,13 +2,7 @@ import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import { Route, Routes } from "react-router-dom";
 import Authentication from "./routes/authentication/authentication.component";
-const Shop = () => {
-  return (
-    <div>
-      <h1>Hi, I am the shop page!</h1>
-    </div>
-  );
-};
+import Shop from "./routes/shop/shop.component";
 const  App = ()=> {
   return (
     <Routes>
