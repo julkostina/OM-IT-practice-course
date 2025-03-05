a1936c6c24eecc4c93d26f5bbd17ae1e6c5dc953
julkostina <julikostina1504@gmail.com>
2025-03-05 22:15:16 +0200

    118-121.Cart-handling
    Added cart item, styles, handled state cart, cart items displayed in cart, total number is displayed

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,20 @@
-import './cart-dropdown.styles.scss'
-import Button from '../button/button.component';
+import React, { useContext } from "react";
+import "./cart-dropdown.styles.scss";
+import Button from "../button/button.component";
+import { CartContext } from "../../contexts/cart.context";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
-    return(
-            <div className="cart-dropdown-container">
-                <div className="cart-items"></div>
-               <Button>GO TO CHECKOUT</Button>
-            </div>
-    )
-}
+    const { cartItems } = useContext(CartContext);
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 export default CartDropdown;
--- crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -3,12 +3,12 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 const CartIcon = () => {
-const {isCartOpen, setIsCartOpen} = useContext(CartContext);
+const {isCartOpen, setIsCartOpen, itemsInCart} = useContext(CartContext);
 const toggleCartIsOpen = () => setIsCartOpen(!isCartOpen);
     return(
         <div className="cart-icon-container" onClick={toggleCartIsOpen}>
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{itemsInCart}</span>
         </div> 
     )
 };
