06aa5990992dec0ad2002c574646eccd3e479540
julkostina <julikostina1504@gmail.com>
2025-03-06 12:32:19 +0200

    132.using-CategoriesMap
    Product context reformed to categories context,  created category preview , shop  component display created CategoriesMap

--- crwn-clothing/src/contexts/categories.context.jsx
+++ crwn-clothing/src/contexts/categories.context.jsx
@@ -2,23 +2,25 @@ import { createContext, useState, useEffect } from "react";
 import {addCollectionAndDocuments, getCategoriesAndDocuments} from "../utils/firebase/firebase.utils";
 
 
-export const ProductsContext = createContext({
-    products: [],
+export const CategoriesContext = createContext({
+    categoriesMap: {},
 });
 
-export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState([]); 
-    const value = { products }; 
+export const CategoriesProvider = ({ children }) => {
+    const [categoriesMap, setCategoriesMap] = useState({}); 
+    const value = { categoriesMap }; 
     useEffect(() => {
          const getCategoryMap = async () => {
          const categoryMap= await getCategoriesAndDocuments();
          console.log(categoryMap);
+         setCategoriesMap(categoryMap)
          }
          getCategoryMap()
+
     }, []);
   return (
-    <ProductsContext.Provider value={ value }>
+    <CategoriesContext.Provider value={ value }>
       {children}
-    </ProductsContext.Provider>
+    </CategoriesContext.Provider>
   );
 } 
--- crwn-clothing/src/routes/shop/shop.component.jsx
+++ crwn-clothing/src/routes/shop/shop.component.jsx
@@ -1,19 +1,26 @@
-import SHOP_DATA from '../../shop-data.js'
-import { useContext } from 'react';
-import { ProductsContext } from '../../contexts/products.context';
-import ProductCard from '../../components/product-card/product-card';
-import './shop.styles.scss';
-const Shop  =()=>{
-    const {products} = useContext(ProductsContext);
-    return(
-        <div className='products-container'> 
-            {
-                products.map((product) => (
-                    <ProductCard key={product.id} product={product}/>
-                ))
-            }
-        </div>
-    )
-}
+import SHOP_DATA from "../../shop-data.js";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+import { Fragment, useContext } from "react";
+import { CategoriesContext } from "../../contexts/categories.context";
+import ProductCard from "../../components/product-card/product-card";
+import "./shop.styles.scss";
+
+const Shop = () => {
+  const {categoriesMap} = useContext(CategoriesContext);
+  return (
+    <Fragment>
+      {Object.keys(categoriesMap).map(title => 
+        <Fragment>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
 
 export default Shop;
