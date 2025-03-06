3ce7c085f3d7bf4a7a7e57280a1d1d2dc2d54ba4
julkostina <julikostina1504@gmail.com>
2025-03-06 22:27:09 +0200

    138.Directory-item-styled

--- crwn-clothing/src/components/directory-item/directory-item.component.jsx
+++ crwn-clothing/src/components/directory-item/directory-item.component.jsx
@@ -1,21 +1,16 @@
-import './directory-item.sytyles.scss'
-
+import {BackgroundImage, Body, DirectoryItemContainer} from'./directory-item.sytyles'
 
 const DirectoryItem = ({ category }) => {
     const { imageUrl, title } = category;
     return (
-      <div className='directory-item-container'>
-        <div
-          className='background-image'
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-          }}
+      <DirectoryItemContainer>
+        <BackgroundImage imageUrl={imageUrl}
         />
-        <div className='body'>
+        <Body>
           <h2>{title}</h2>
           <p>Shop Now</p>
-        </div>
-      </div>
+        </Body>
+      </DirectoryItemContainer>
     );
   };
   
