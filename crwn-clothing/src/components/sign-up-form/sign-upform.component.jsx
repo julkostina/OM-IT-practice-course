d20ffcadf4747e6b05ff07e0edf0f707d5a22f8b
julkostina <julikostina1504@gmail.com>
2025-03-06 23:11:54 +0200

    139.Sign-up-form-styled

--- crwn-clothing/src/components/sign-up-form/sign-upform.component.jsx
+++ crwn-clothing/src/components/sign-up-form/sign-upform.component.jsx
@@ -3,7 +3,7 @@ import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
-import './sign-up-form.styles.scss';
+import {SignUpContainer} from './sign-up-form.styles';
 import FormInput from "../form-input/form-input.component"
 import Button from "../button/button.component";
 import { BUTTON_TYPE_CLASSES } from "../button/button.component";
@@ -47,7 +47,7 @@ const SignUpForm = () => {
   };
 
   return (
-    <div class='sign-up-container'>
+    <SignUpContainer>
       <h2>Don`t have an account?</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -85,7 +85,7 @@ const SignUpForm = () => {
         />
         <Button buttonType={BUTTON_TYPE_CLASSES.base } type="submit" >Sign Up</Button>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 
