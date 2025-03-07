4112de0e92440cd85b581f19d198b29f216dc024
julkostina <julikostina1504@gmail.com>
2025-03-07 23:02:47 +0200

    Play 164.Reselect-Library
    Reselect explained, category selector momoized using CreateSelector

--- crwn-clothing/src/store/categories/category.selector.js
+++ crwn-clothing/src/store/categories/category.selector.js
@@ -1,7 +1,15 @@
-export const selectCategoriesMap = (state) =>{
-    console.log('selector fired')
-  return state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state)=> state.categories;
+
+export const selectCategories = createSelector([selectCategoryReducer],(categoriesSlice)=>{
+   return categoriesSlice.categories
+})
+
+export const selectCategoriesMap = createSelector([selectCategories],(categories)=>{
+  return categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
     return acc;
-  }, {});}
+  }, {});
+})
