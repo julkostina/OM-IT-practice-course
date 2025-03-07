8dcbc208d150be39973e7d217a356261b0fe03c1
julkostina <julikostina1504@gmail.com>
2025-03-07 13:51:21 +0200

    147-149.useReducer
    the hook added to userContext, cartContext, created reducer.utils for optimization

--- crwn-clothing/src/contexts/user.context.jsx
+++ crwn-clothing/src/contexts/user.context.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useReducer} from "react";
 import {onAuthStateChangedListener, createUserDocumentFromAuth} from "../utils/firebase/firebase.utils";
+import {createAction} from '../utils/reducer/reducer.utils';
 
 export const UserContext = createContext({
     currentUser: null,
@@ -10,8 +11,6 @@ export const USER_ACTION_TYPES = {
     'SET_CURRENT_USER' : 'SET_CURRENT_USER'
 }
 const userReducer = (state , action) => {
-    console.log("dispatched ")
-    console.log(action);
     const {type, payload} = action;
 
 
@@ -36,7 +35,7 @@ export const UserProvider = ({children}) => {
     console.log(currentUser);
 
     const setCurrentUser = (user) => {
-        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user});
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
     }
      const value  = {currentUser, setCurrentUser};
      useEffect(()=>{
