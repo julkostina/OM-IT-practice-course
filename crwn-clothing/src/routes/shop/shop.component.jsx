9af200e6e5a12b1d9b5d7e2c6281afe16febf9f5
julkostina <julikostina1504@gmail.com>
2025-03-09 15:19:20 +0200

    171-173.Redux-Thunk
    Explained Redux-Thunk, aded spinner

--- crwn-clothing/src/routes/shop/shop.component.jsx
+++ crwn-clothing/src/routes/shop/shop.component.jsx
@@ -1,6 +1,5 @@
 import { useEffect} from "react";
 import { useDispatch } from "react-redux";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { fetchCategoriesAsync } from "../../store/categories/category.action";
 import {Routes, Route} from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
--- crwn-clothing/src/store/categories/category.selector.js
+++ crwn-clothing/src/store/categories/category.selector.js
@@ -1,15 +1,26 @@
 import { createSelector } from "reselect";
 
-const selectCategoryReducer = (state)=> state.categories;
+const selectCategoryReducer = (state) => state.categories;
 
-export const selectCategories = createSelector([selectCategoryReducer],(categoriesSlice)=>{
-   return categoriesSlice.categories
-})
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => {
+    return categoriesSlice.categories;
+  }
+);
 
-export const selectCategoriesMap = createSelector([selectCategories],(categories)=>{
-  return categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-})
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
+);
