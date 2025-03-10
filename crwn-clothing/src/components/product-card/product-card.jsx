13354736ff821ea771c54843192b3629bee039e5
julkostina <julikostina1504@gmail.com>
2025-03-10 21:34:02 +0200

    188-189.Categories-Cart
    Categories and Cart components have the appropriate structure for using redux-toolkit

--- crwn-clothing/src/components/product-card/product-card.jsx
+++ crwn-clothing/src/components/product-card/product-card.jsx
@@ -1,14 +1,12 @@
-import { useDispatch,useSelector } from 'react-redux';
-import {selectCartItems} from '../../store/cart/cart.selector';
-import { addItemToCart } from '../../store/cart/cart.action';
+import { useDispatch } from 'react-redux';
+import { addItemToCart } from '../../store/cart/cart.reducer';
 import { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import {Name, Price, Footer, ProductCardContainer}from './product-card.styles'
 import Button from '../button/button.component';
 const ProductCard = ({product}) => {
     const {name, price, imageUrl} = product; 
     const dispatch = useDispatch();
-    const cartItems = useSelector(selectCartItems);
-    const addProductToCart = ()=> dispatch(addItemToCart(cartItems, product));
+    const addProductToCart = ()=> dispatch(addItemToCart( product));
 
     return (
         <ProductCardContainer>
