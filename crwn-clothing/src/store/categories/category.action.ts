45becc3e877f3a6b36ed3f8ac11ad9d397fab117
julkostina <julikostina1504@gmail.com>
2025-03-11 17:46:01 +0200

    207-208.Category-Action
    defining types of actions to be returned from category.action, creating union of actions

---
+++ crwn-clothing/src/store/categories/category.action.ts
@@ -0,0 +1,36 @@
+import {
+  createAction,
+  Action,
+  ActionWithPayload,
+} from "../../utils/reducer/reducer.utils";
+import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
+
+export type FetchCategoriesSuccess = ActionWithPayload<
+  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+  Category[]
+>;
+export type fetchCategoriesStart =
+  Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
+export type FetchCategoriesFailed = ActionWithPayload<
+  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+  Error
+>;
+export type CategoryAction =
+  | FetchCategoriesSuccess
+  | fetchCategoriesStart
+  | FetchCategoriesFailed;
+export const FetchCategoriesStart = (): fetchCategoriesStart => {
+  return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+};
+
+export const fetchCategoriesSuccess = (
+  categoriesArray: Category[]
+): FetchCategoriesSuccess => {
+  return createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+};
+export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed => {
+  return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+};
