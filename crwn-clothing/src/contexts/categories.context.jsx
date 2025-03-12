3160d22ccec301aa7776806d90a03563cca074b8
julkostina <julikostina1504@gmail.com>
2025-03-12 18:50:26 +0200

    234.Updating-Components-To-Consume-GraphQL-Values
    added missing spinner, fetched collections

--- crwn-clothing/src/contexts/categories.context.jsx
+++ crwn-clothing/src/contexts/categories.context.jsx
@@ -21,9 +21,19 @@ const COLLECTIONS = gql`
 `;
 export const CategoriesProvider = ({ children }) => {
   const { loading, error, data } = useQuery(COLLECTIONS);
-  console.log(data);
   const [categoriesMap, setCategoriesMap] = useState({});
-  const value = { categoriesMap };
+  useEffect(()=>{
+    if(data){
+      const {collections} = data;
+      const collectionsMap = collections.reduce((acc, collection)=>{
+        const {title, items} = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+      setCategoriesMap(collectionsMap);
+    }
+  }, [data])
+  const value = { categoriesMap,loading};
 
   return (
     <CategoriesContext.Provider value={value}>
