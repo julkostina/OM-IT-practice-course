277afaab33593aeb2552e25a871856592139cd4a
julkostina <julikostina1504@gmail.com>
2025-03-12 22:07:29 +0200

    235.Variables-in-useQuery&Caching
    Shown how can be category using Apollo

--- crwn-clothing/src/routes/category/category.component.jsx
+++ crwn-clothing/src/routes/category/category.component.jsx
@@ -1,16 +1,55 @@
 import { useParams } from "react-router-dom";
 import "./category.styles.scss";
-import { useContext, useState, useEffect,Fragment } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
+import React from "react";
+import {  useState,Fragment } from "react";
 import ProductCard from "../../components/product-card/product-card";
+import {gql, useQuery, useMutation} from "@apollo/client";
 
+const GET_CATEGORY = gql`
+  query ($title: String) {
+    getCollectionsByTitle(title: $title) {
+      title
+      id
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
+const SET_CATEGORY = gql`
+  mutation ($category: category!){
+    addCategory(category: $category){
+      id
+      title
+      items{
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }`
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
-  useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+  // const [addCategory, {loading, error, data} ] = useMutation(SET_CATEGORY);
+const {loading, error, data} = useQuery(GET_CATEGORY, {
+variables:{
+  title: category
+},
+})
+console.log(data);
+React.useEffect(()=>{
+if(data){
+  const {getCollectionsByTitle:{items}} = data;
+  setProducts(items);
+}
+}, [data, category])
+  const [products, setProducts] = useState([]);
+
   return (
     <Fragment>
     <h2 className="category-title">{category.toLocaleUpperCase()}</h2>
