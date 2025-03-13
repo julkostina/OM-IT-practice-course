1ed520e9cb173fa302cdd7965c0d678005605579
julkostina <julikostina1504@gmail.com>
2025-03-13 20:20:12 +0200

    280.Writing-Tests
    group tests wit describe, fix file extension

--- test/script.test.js
+++ test/script.test.js
@@ -1,7 +1,22 @@
 const googleSearch = require("./script");
 
 dbMock = ["dog.com", "cheesepuff.com", "disney.com", "dogpictures.com"];
-it("is a searching google", () => {
-  expect(googleSearch("testtest", dbMock)).toEqual([]);
-  expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+describe("googleSearch", () => {
+  it("this is a test", () => {
+    expect("hello").toBe("hello");
+  });
+
+  it("is searching google", () => {
+    expect(googleSearch("testtest", dbMock)).toEqual([]);
+    expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+  });
+
+  it("work with undefined and null input", () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it("does not return more than 3 matches", () => {
+    expect(googleSearch(".com", dbMock).length).toEqual(3);
+  });
 });
