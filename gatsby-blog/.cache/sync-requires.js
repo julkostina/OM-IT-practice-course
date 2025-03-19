6bb1d009c847935b1cb2d4767c9d4cb669d808ec
julkostina <julikostina1504@gmail.com>
2025-03-19 10:08:32 +0200

    Adding GatsByJS files

---
+++ gatsby-blog/.cache/sync-requires.js
@@ -0,0 +1,15 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("D:\\Projects\\OM-IT-practice-course\\gatsby-blog\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("D:\\Projects\\OM-IT-practice-course\\gatsby-blog\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("D:\\Projects\\OM-IT-practice-course\\gatsby-blog\\src\\pages\\index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("D:\\Projects\\OM-IT-practice-course\\gatsby-blog\\src\\pages\\page-2.js")),
+  "component---src-pages-using-ssr-js": preferDefault(require("D:\\Projects\\OM-IT-practice-course\\gatsby-blog\\src\\pages\\using-ssr.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("D:\\Projects\\OM-IT-practice-course\\gatsby-blog\\src\\pages\\using-typescript.tsx")),
+  "component---src-templates-using-dsg-js": preferDefault(require("D:\\Projects\\OM-IT-practice-course\\gatsby-blog\\src\\templates\\using-dsg.js"))
+}
+
